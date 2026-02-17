433812640f4fc64905ec18309e38e719ded74d55
ramklk <ramprasadvkulkarni@gmail.com>
2026-02-17 16:53:03 +0530

    Initial full stack setup

---
+++ backend/routes/habitRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createHabit,
+  getUserHabits,
+  markHabitDone,
+  deleteHabit
+} = require("../controllers/habitcontroller");
+
+const { getStats } = require("../controllers/habitcontroller");
+
+const authMiddleware = require("../middleware/authMiddleware");
+
+
+/**
+ * 📝 Create New Habit
+ * POST /api/habits
+ */
+router.post("/", authMiddleware, createHabit);
+
+/**
+ * 📋 Get All Habits of Logged-in User
+ * GET /api/habits
+ */
+router.get("/", authMiddleware, getUserHabits);
+
+router.get("/stats", authMiddleware, getStats);
+
+/**
+ * 🔥 Mark Habit as Done (Check-in)
+ * POST /api/habits/:id/checkin
+ */
+router.post("/:id/checkin", authMiddleware, markHabitDone);
+
+/**
+ * ❌ Delete Habit
+ * DELETE /api/habits/:id
+ */
+router.delete("/:id", authMiddleware, deleteHabit);
+
+module.exports = router;
